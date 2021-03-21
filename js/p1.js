034ba0817c66916f41096912fda80282f24793d4
Tom-Cao <77942451+Tom-Cao@users.noreply.github.com>
2021-03-21 18:48:41 -0400

    Update p1.js

--- js/p1.js
+++ js/p1.js
@@ -1,4 +1,4 @@
 // Change the weekly deal price according to the real price
 let itemPrice1 = document.getElementsByClassName("price1");
-itemPrice1[0].innerHTML = "$3.99 each";
-itemPrice1[1].innerHTML = "$3.99";
+itemPrice1[0].innerHTML = "$4.99 each";
+itemPrice1[1].innerHTML = "$4.99";
